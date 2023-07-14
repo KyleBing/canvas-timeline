15a2b2cfd1491f5579503d1377f8aee2162657c5
邴新科-工作 <kylebing@163.com>
2023-07-14 17:55:21 +0800

    init

---
+++ canvas-timeline.js
@@ -0,0 +1,485 @@
+/**
+ * 我的所有物品
+ * Timeline
+ * @author: KyleBing(kylebing@163.com)
+ * @github: https://github.com/KyleBing/canvas-mine
+ * @date-init: 2023-07-10
+ * @date-update: 2023-07-13
+ * @version: v0.0.1
+ * @platform: NPM
+ */
+
+class CanvasTimeline {
+    /**
+     * Timeline
+     * @param name {String}主题名
+     * @param diaries {[]} 内容
+     * @param columnCount {Number} 展示为多少列
+     * @param columnOffsetX {Number} 列之间的间隔
+     * @param isShowSerialNumber {Boolean} 是否显示序号
+     * @param isShowCanvasInfo {Boolean} 是否显示 canvas 信息
+     */
+    constructor(
+        name,
+        diaries,
+        columnCount,
+        columnOffsetX,
+        isShowSerialNumber,
+        isShowCanvasInfo
+    )
+    {
+        this.isPlaying = false // 默认自动播放
+        this.isShowCanvasInfo = isShowCanvasInfo
+        this.isShowSerialNumber = isShowSerialNumber
+
+        this.columnCount = columnCount || 2       // 展示为多少列
+        this.columnOffsetX = columnOffsetX || 700 // 列之间的间隔
+
+        this.columnOffsetXFirst = 400 // 第一列的开始，偏移量
+
+        this.baseX = 600
+        this.bgColor = 'white'
+
+        this.frame = {
+            width : 1920 * 2,
+            height: 1080 * 2,
+        }
+        this.center=  {
+            x: 600,
+            y: 150
+        }
+
+        this.scrollOffset = 0
+
+        this.diaries = diaries
+        this.timeLine = 0
+        this.mouseX = 0
+        this.mouseY = 0
+        this.lastTime = new Date().getTime() // 用于计算每帧用时
+
+        this.init()
+        window.onresize = () => {
+            this.frame.height = innerHeight * 2
+            this.frame.width = innerWidth * 2
+            let canvasLayer = document.querySelector('canvas')
+            this.updateFrameAttribute(canvasLayer)
+            // this.init()
+        }
+
+        document.documentElement.addEventListener('mousemove', event => {
+            this.mouseX = event.x
+            this.mouseY = event.y
+
+            if (event.ctrlKey){
+                this.scrollOffset = this.scrollOffset - 3
+            } else {
+                this.scrollOffset = this.scrollOffset + 3
+            }
+
+            // fill background
+            let canvasLayer = document.getElementById('canvasLayer')
+            let c = canvasLayer.getContext('2d')
+            this.draw()
+
+            c.save()
+            c.lineWidth = 1
+            c.strokeStyle = 'red'
+            c.beginPath()
+            c.moveTo(0, this.mouseY * 2)
+            c.lineTo(this.frame.width * 2, this.mouseY * 2)
+            c.stroke()
+
+            c.beginPath()
+            c.moveTo(this.mouseX * 2, 0)
+            c.lineTo(this.mouseX * 2, this.frame.height)
+            c.stroke()
+
+            c.restore()
+        })
+
+        document.addEventListener('scroll', event => {
+            console.log(event)
+        })
+    }
+
+    updateFrameAttribute(canvasLayer){
+        canvasLayer.setAttribute('id', 'canvasLayer')
+        canvasLayer.setAttribute('width', this.frame.width)
+        canvasLayer.setAttribute('height', this.frame.height)
+        canvasLayer.style.width = `${this.frame.width / 2}px`
+        canvasLayer.style.height = `${this.frame.height / 2}px`
+        canvasLayer.style.zIndex = '-3'
+        canvasLayer.style.userSelect = 'none'
+        canvasLayer.style.position = 'fixed'
+        canvasLayer.style.top = '0'
+        canvasLayer.style.left = '0'
+        canvasLayer.imageSmoothingEnabled = true
+
+        // fill background
+        let ctx = canvasLayer.getContext('2d')
+        ctx.fillStyle = this.bgColor
+        ctx.fill()
+
+        this.draw()
+    }
+
+    init(){
+        this.frame.height = document.documentElement.clientHeight * 2
+        this.frame.width = document.documentElement.clientWidth * 2
+
+        this.center = {
+            x: (this.frame.width - (this.columnOffsetX - 250) * 2 * this.columnCount) / 2, // 300 大约是两个列之间重叠的部分
+            y: this.frame.height / 2
+        }
+
+        let canvasLayer = document.createElement("canvas")
+        document.documentElement.append(canvasLayer)
+        this.updateFrameAttribute(canvasLayer)
+
+        this.draw()
+    }
+
+    draw(scrollOffset) {
+        this.timeLine = this.timeLine + 1
+        let canvasLayer = document.getElementById('canvasLayer')
+        let c = canvasLayer.getContext('2d')
+        c.clearRect(0,0,this.frame.width, this.frame.height)
+
+        // 背景
+        c.save()
+        c.fillStyle = 'white'
+        c.fillRect(0,0,this.frame.width, this.frame.height)
+        c.restore()
+
+        let centerX = this.baseX
+        c.strokeStyle = 'black'
+        c.lineWidth = 10
+        c.beginPath()
+        c.moveTo(centerX,0)
+        c.lineTo(centerX, this.frame.height)
+        c.stroke()
+
+        for (let i=0;i<this.diaries.length;i++){
+            this.drawDiary(c,this.diaries[i],  100 * (i + 1) - this.scrollOffset)
+        }
+
+        // 展示 canvas 数据
+        if (this.isShowCanvasInfo) {
+            showCanvasInfo(c, this.timeLine, this.frame)
+        }
+
+        if (this.isPlaying) {
+            window.requestAnimationFrame(() => {
+                this.draw()
+            })
+        }
+    }
+
+    drawDiary(c,diary, positionY){
+        c.save()
+        c.fillStyle = 'black'
+        c.strokeStyle = 'black'
+        c.lineWidth = 2
+
+        c.textBaseline = 'middle'
+        c.beginPath()
+        c.moveTo(this.baseX, positionY)
+        c.lineTo(this.baseX + 200, positionY)
+        c.stroke()
+
+        // 标题
+        c.font = '28px 微软雅黑'
+        c.textAlign = 'left'
+        c.fillText(diary.title, this.baseX + 200 + 20, positionY)
+
+        // 标题
+        c.font = '24px 微软雅黑'
+        c.textAlign = 'left'
+        c.fillStyle = '#aaa'
+        if (diary.content.length > 60){
+            c.fillText(diary.content.substring(0,60) + '...', this.baseX + 200 + 20, positionY + 35)
+        } else {
+            c.fillText(diary.content, this.baseX + 200 + 20, positionY + 35)
+        }
+
+
+        // 日期
+        c.font = '23px 微软雅黑'
+        c.fillStyle = '#aaa'
+        c.fillText( (dateFormatter(new Date(diary.date))),this.baseX - 280, positionY,)
+
+
+
+        c.restore()
+    }
+
+
+    animationStart(){
+        if (this.isPlaying){
+
+        } else {
+            this.isPlaying = true
+            this.draw()
+        }
+    }
+    animationStop(){
+        this.isPlaying = false
+    }
+
+    destroy(){
+        this.isPlaying = false
+        let canvasLayer = document.getElementById('canvasLayer')
+        canvasLayer.remove()
+        console.log('动画已停止')
+    }
+}
+
+/**
+ * ## 显示时间标线序号
+ * @param ctx
+ * @param timeline {''}
+ * @param frame {{width, height}}
+ */
+function showCanvasInfo(ctx, timeline, frame){
+    ctx.save()
+    ctx.beginPath()
+    ctx.fillStyle = 'white'
+    ctx.font = '20px sans-serf'
+    ctx.fillRect(10, frame.height - 53, 220, 30)
+    let currentTime =  new Date().getTime()
+    ctx.fillStyle = 'black'
+    ctx.fillText(`${currentTime - this.lastTime} ms/frame  |  ${timeline} 帧`, 20, frame.height - 32)
+    this.lastTime = currentTime
+    ctx.restore()
+}
+
+function getLines(ctx, text, maxWidth) {
+    var words = text.split(" ");
+    var lines = [];
+    var currentLine = words[0];
+
+    for (var i = 1; i < words.length; i++) {
+        var word = words[i];
+        var width = ctx.measureText(currentLine + " " + word).width;
+        if (width < maxWidth) {
+            currentLine += " " + word;
+        } else {
+            lines.push(currentLine);
+            currentLine = word;
+        }
+    }
+    lines.push(currentLine);
+    return lines;
+}
+
+/**
+ *
+ * @param ctx
+ * @param center
+ * @param radius {Number}
+ * @param color {String}
+ */
+
+/**
+ * ## 画点
+ * @param ctx
+ * @param center {{x: Number,y: Number}}
+ * @param radius  {Number}
+ * @param lineWidth {Number}
+ * @param fillColor  {String}
+ * @param strokeColor {String}
+ */
+function drawDot(ctx, center, radius, lineWidth, fillColor, strokeColor){
+    ctx.save()
+    ctx.beginPath()
+    ctx.moveTo(center.x + radius, center.y)
+    ctx.lineWidth = lineWidth || 0
+    ctx.strokeStyle = fillColor || 'black'
+    ctx.fillStyle =  strokeColor || 'white'
+    ctx.arc(center.x, center.y, radius,0, Math.PI * 2 )
+    ctx.closePath()
+    ctx.fill()
+    ctx.stroke()
+    ctx.restore()
+}
+
+/**
+ * ## 获取第 index 个元素的 y 位置
+ * @param middleLineY {{x: Number, y: Number}} 中心线的 y 位置
+ * @param itemSize {Number}元素数量
+ * @param gap {Number} 每个元素之间的间隔
+ * @param index {Number} 第几个元素的位置
+ */
+function getYPositionOf(middleLineY, itemSize, gap, index){
+    let gapCount = itemSize - 1 // gap 总数量
+    let middleIndex = gapCount / 2
+    if (index >= middleIndex){
+        return middleLineY + (index - middleIndex) * gap
+    } else {
+        return middleLineY - (middleIndex - index) * gap
+    }
+}
+
+/**
+ * ## 在 a 与 d 点之间线一条带圆角的拆线
+ * @param ctx canvas.context
+ * @param pointA {{x: Number, y: Number}} 起点坐标
+ * @param pointD {{x: Number, y: Number}} 末端坐标
+ * @param radius  { Number } 圆角半径
+ * @param endLineLength  { Number } 末端线段长度
+ * @param lineWidth { Number } 线段宽度
+ * @param lineColor  { String } 线段颜色
+ */
+function drawArcLine(ctx, pointA, pointD, radius,  endLineLength, lineWidth, lineColor){
+    ctx.save()
+    ctx.lineCap = 'round'
+    ctx.beginPath()
+    ctx.lineJoin = 'round'
+    ctx.moveTo(pointA.x, pointA.y)
+    let foldX = pointA.x + (pointD.x - pointA.x - endLineLength)
+    ctx.arcTo(
+        foldX,
+        pointA.y,
+        foldX,
+        pointD.y,
+        radius
+    )
+    ctx.arcTo(
+        foldX,
+        pointD.y,
+        pointD.x,
+        pointD.y,
+        radius
+    )
+    ctx.lineTo(pointD.x, pointD.y)
+    ctx.strokeStyle = lineColor
+    ctx.lineWidth = lineWidth
+    ctx.stroke()
+    ctx.restore()
+    return foldX
+}
+
+
+function getColor(timeLine){
+    return `hsla(${timeLine%360 + 200},150%,50%,1)`
+}
+
+/**
+ * 输出随机 1 或 -1
+ * @returns {number}
+ */
+function randomDirection(){
+    let random = Math.random()
+    if (random > 0.5){
+        return 1
+    } else {
+        return -1
+    }
+}
+
+function randomPosition(width, height){
+    return [
+        Number((width * Math.random()).toFixed(0)),
+        Number((height * Math.random()).toFixed(0))
+    ]
+}
+
+/**
+ * 数组乱序算法
+ * @param arr
+ * @return {*}
+ */
+function shuffle(arr) {
+    let length = arr.length,
+        r = length,
+        rand = 0;
+
+    while (r) {
+        rand = Math.floor(Math.random() * r--);
+        [arr[r], arr[rand]] = [arr[rand], arr[r]];
+    }
+    return arr;
+}
+
+/**
+ * 生成随机整数
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+function randomInt(min, max){
+    return Number((Math.random() * (max - min) + min).toFixed(0))
+}
+
+/**
+ * 生成随机整数
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+function randomFloat(min, max){
+    return Number(Math.random() * (max - min) + min)
+}
+
+
+// 格式化时间，输出字符串
+function dateFormatter(date, formatString) {
+    formatString = formatString || 'yyyy-MM-dd hh:mm:ss'
+    let dateRegArray = {
+        "M+": date.getMonth() + 1,                      // 月份
+        "d+": date.getDate(),                           // 日
+        "h+": date.getHours(),                          // 小时
+        "m+": date.getMinutes(),                        // 分
+        "s+": date.getSeconds(),                        // 秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), // 季度
+        "S": date.getMilliseconds()                     // 毫秒
+    }
+    if (/(y+)/.test(formatString)) {
+        formatString = formatString.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length))
+    }
+    for (let section in dateRegArray) {
+        if (new RegExp("(" + section + ")").test(formatString)) {
+            formatString = formatString.replace(RegExp.$1, (RegExp.$1.length === 1) ? (dateRegArray[section]) : (("00" + dateRegArray[section]).substr(("" + dateRegArray[section]).length)))
+        }
+    }
+    return formatString
+}
+
+function dateProcess(dateString) {
+    let date = new Date(dateString)
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
+    let hour = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let week = date.getDay()
+    let timeLabel = ''
+    if (hour >= 23 && hour < 24 || hour <= 3 && hour >= 0) {
+        timeLabel = '深夜'
+    } else if (hour >= 19 && hour < 23) {
+        timeLabel = '晚上'
+    } else if (hour >= 14 && hour < 19) {
+        timeLabel = '傍晚'
+    } else if (hour >= 11 && hour < 14) {
+        timeLabel = '中午'
+    } else if (hour >= 6 && hour < 11) {
+        timeLabel = '早上'
+    } else if (hour >= 3 && hour < 6) {
+        timeLabel = '凌晨'
+    }
+
+    return {
+        year,
+        day,
+        month,
+        weekday: WEEKDAY[week],
+        weekShort: WEEKDAY_SHORT[week],
+        dateShort:`${padNumberWith0(month)}-${padNumberWith0(day)}`,
+        date:`${padNumberWith0(month)}月${padNumberWith0(day)}日`,
+        dateFull: `${year}年${month}月${day}日`,
+        dateFullSlash: `${year}/${month}/${day}`,
+        timeLabel: timeLabel,
+        time: `${padNumberWith0(hour)}:${padNumberWith0(minutes)}`
+    }
+}
